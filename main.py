00dd01ac78d847e7713e2f24730c6af343f46fde
joshvand <joshvand@woodland.on.ca>
2022-11-28 10:34:01 -0500

    new test at github

--- main.py
+++ main.py
@@ -2,5 +2,17 @@ import os, sys
 
 from PIL import Image
 im = Image.open("Triforce.png")
+print(im.format, im.size, im.mode)
+
+def roll(im, delta):
+    xsize, ysize, = im.size
+
+    delta = delta % xsize
+    if delta == 0:
+        return im
+
+    part1 = im.crop((0, 0, delta, ysize))
+    part2 = im.crop((delta))
+
 
 
