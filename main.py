a0b2607072711fd0799c9f305ac85f16cc998ab6
joshvand <joshvand@woodland.on.ca>
2022-11-30 12:27:52 -0500

    work finished november 30

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
+from PIL import Image
 import os, sys
 
-from PIL import Image
-im = Image.open("Triforce.png")
-print(im.format, im.size, im.mode)
-
-def roll(im, delta):
-    xsize, ysize, = im.size
-
-    delta = delta % xsize
-    if delta == 0:
-        return im
-
-    part1 = im.crop((0, 0, delta, ysize))
-    part2 = im.crop((delta))
-
-
-
+image =
+picture = Image.open(image)
---
+++ test.py
@@ -0,0 +1,20 @@
+import os, sys
+
+from PIL import Image
+im = Image.open("Triforce.png")
+print(im.format, im.size, im.mode)
+
+josh_png = Image.open("Josh.png")
+def merge(im1, im2):
+    w = max(im1.size[0], im2.size[0])
+    h = max(im1.size[1], im2.size[1])
+    im = Image.new("RGBA", (w, h))
+
+    im.paste(im1)
+    im.paste(im2)
+
+    return im
+
+im = merge(im, josh_png)
+
+im.show(im)
